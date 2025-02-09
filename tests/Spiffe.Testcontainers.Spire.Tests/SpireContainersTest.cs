b07266a5fba375cbf1065cce87c3a2c3c48b64c1
Alexander Vurganov <alex.vurganov@gmail.com>
2025-02-09 23:53:31 +0000

    WIP

--- tests/Spiffe.Testcontainers.Spire.Tests/SpireContainersTest.cs
+++ tests/Spiffe.Testcontainers.Spire.Tests/SpireContainersTest.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.IO;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices.Marshalling;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
 using DotNet.Testcontainers.Builders;
 using DotNet.Testcontainers.Networks;
@@ -124,13 +128,13 @@ public class SpireContainersTest
         await w.AssertLogAsync("Received 1 svid after", 10);
     }
 
-    [Fact(Timeout = 60_000)]
+    [Fact(Timeout = 180_000)]
     public async Task StartFederationTest()
     {
-        var output = Consume.RedirectStdoutAndStderrToConsole();
-        var net = new NetworkBuilder().WithName( "spire-federation-" + Guid.NewGuid().ToString("D")).Build();
+        using var output = Consume.RedirectStdoutAndStderrToConsole();
+        await using var net = new NetworkBuilder().WithName("spire-federation-" + Guid.NewGuid().ToString("D")).Build();
 
-        async Task<(SpireServerContainer Srv, SpireAgentContainer Agt, Func<Task> Close)> Start(ServerConf sc, AgentConf ac)
+        async Task<(SpireServerContainer Srv, Func<Task> Close)> Start(ServerConf sc, AgentConf ac)
         {
             var td = sc.TrustDomain;
             var vol = new VolumeBuilder().WithName(td + "-" + Guid.NewGuid().ToString("D")).Build();
@@ -161,18 +165,33 @@ public class SpireContainersTest
                 "-selector", "docker:label:com.example:workload"
             ]);
 
-            return (s, a, async () =>
+            return (s, async () =>
             {
                 try { await a.DisposeAsync(); } catch {}
                 try { await s.DisposeAsync(); } catch {}
                 try { await vol.DisposeAsync(); } catch {}
-                try { await net.DisposeAsync(); } catch {}
             });
         }
 
-        Func<Task> Close1 = () => Task.CompletedTask;
-        Func<Task> Close2 = () => Task.CompletedTask;
+        async Task Federate(SpireServerContainer from, SpireServerContainer to, string trustDomain)
+        {
+            var resp = await from.ExecAsync([
+                "/opt/spire/bin/spire-server", "bundle", "show", "-format", "spiffe"
+            ]);
+            Assert.Empty(resp.Stderr);
 
+            var bundle = Encoding.UTF8.GetBytes(resp.Stdout);
+            var bundlePath = $"/tmp/{trustDomain}.bundle";
+            await to.CopyAsync(bundle, bundlePath);
+            resp = await to.ExecAsync([
+                "/opt/spire/bin/spire-server", "bundle", "set", "-format", "spiffe", "-id", $"spiffe://{trustDomain}", "-path", bundlePath
+            ]);
+            Assert.Empty(resp.Stderr);
+            Assert.Contains("bundle set", resp.Stdout);
+        }
+
+        Func<Task> close1 = () => Task.CompletedTask;
+        Func<Task> close2 = () => Task.CompletedTask;
         try
         {
             var sc1 = new ServerConf
@@ -180,27 +199,46 @@ public class SpireContainersTest
                 TrustDomain = "example1.org",
                 Federation = new ServerConfFederation
                 {
-                    FederatesWith = [ new() { TrustDomain = "example2.org", Host = "spire-server2", } ]
+                    Port = 8082,
+                    FederatesWith = [ new() { TrustDomain = "example2.org", Host = "spire-server2", Port = 8082, } ]
                 }
             };
-            var ac1 = new AgentConf{ TrustDomain = "example1.org", ServerAddress = "spire-server1", };
-            (SpireServerContainer s1, SpireAgentContainer a1, Close1) = await Start(sc1, ac1);
-
             var sc2 = new ServerConf
             {
                 TrustDomain = "example2.org",
                 Federation = new ServerConfFederation
                 {
-                    FederatesWith = [ new() { TrustDomain = "example1.org", Host = "spire-server1", } ]
+                    Port = 8082,
+                    FederatesWith = [ new() { TrustDomain = "example1.org", Host = "spire-server1", Port = 8082, } ]
                 }
             };
+            var ac1 = new AgentConf{ TrustDomain = "example1.org", ServerAddress = "spire-server1", };
             var ac2 = new AgentConf{ TrustDomain = "example2.org", ServerAddress = "spire-server2", };
-            (SpireServerContainer s2, SpireAgentContainer a2, Close2) = await Start(sc2, ac2);
+
+            var td = await Task.WhenAll(Start(sc1, ac1), Start(sc2, ac2));
+            close1 = td[0].Close;
+            close2 = td[1].Close;
+
+            await Task.WhenAll(
+                Federate(td[0].Srv, td[1].Srv, "example1.org"),
+                Federate(td[1].Srv, td[0].Srv, "example2.org")
+            );
+
+            // Bundle refresh interval
+            await Task.Delay(60_000);
+            await Task.WhenAll(
+                td[0].Srv.AssertLogAsync($"level=info msg=\"Bundle refreshed\" subsystem_name=bundle_client trust_domain=example2.org", 60),
+                td[1].Srv.AssertLogAsync($"level=info msg=\"Bundle refreshed\" subsystem_name=bundle_client trust_domain=example1.org", 60)
+            );
+        }
+        catch (Exception e)
+        {
+            Assert.Fail(e.ToString());
         }
         finally
         {
-            await Close1();
-            await Close2();
+            await close1();
+            await close2();
         }
     }
 }
